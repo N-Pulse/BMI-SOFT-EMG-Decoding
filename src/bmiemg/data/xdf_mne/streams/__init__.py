2f8de615bc3a51d7cc3ee1f66ac19ef6cc1e67e7
GuilhermeCF <guicostaferreira.2003@gmail.com>
2026-05-07 17:06:13 +0200

    remove: cleared old broken code

--- src/bmiemg/data/xdf_mne/streams/__init__.py
+++
@@ -1,7 +0,0 @@
-from .EEGStream import EEGStream
-from .EMGStream import EMGStream
-
-__all__ = [
-    "EEGStream",
-    "EMGStream"
-]
--- src/bmiemg/data/xdf_mne/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from .stream_finder import find_stream
-from .stream_splitter import split_stream
-
-__all__ = [
-    "find_stream",
-    "split_stream"
-]
